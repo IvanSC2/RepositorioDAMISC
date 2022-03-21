8abc41b4167b4e02c05c777308c30d4e7fccef81
IvanSC2 <ivansanchezcuerva@gmail.com>
2022-03-21 19:39:37 +0100

    He añadido un print que dice, Modificao

---
+++ CoordenadasPY.py
@@ -0,0 +1,7 @@
+import math
+x1= int(input("Dime la coordenada x del primer punto: "))
+x2= int(input("Dime la coordenada x del primer punto: "))
+y1= int(input("Dime la coordenada y del segundo punto: "))
+y2= int(input("Dime la coordenada y del segundo punto: "))
+print("la distancia es", math.sqrt((x2-x1)**2 - (y2-y1)**2))
+print("Modificao")
